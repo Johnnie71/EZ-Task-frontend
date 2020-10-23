eab2bd4b577ff50019712bf6833efbed2ab19459
johnnie71 <johnnie.agonz71@gmail.com>
2020-10-23 10:02:45 -0400

    polishing and last minute fixes

--- src/index.js
+++ src/index.js
@@ -2,91 +2,186 @@
 document.addEventListener("DOMContentLoaded", () => {
 
 	const baseUrl = "http://localhost:3000/api/v1/tasks/"
+
+
+	const addTaskToDom = (task) => {
+		const taskUl = document.querySelector('#list')
+		const taskDiv = document.createElement('div')
+		taskDiv.classList.add('task-div')
+		taskDiv.dataset.id = task.id
+		const taskP = document.createElement('li')
+		taskP.innerHTML =`
+		<strong>${task.subject}</strong>
+		<p>${task.content}</p>
+		<div class="edit" data-id="${task.id}">✏️</div>
+		<div class="delete" data-id="${task.id}">x</div>
+		`
+			taskDiv.appendChild(taskP)
+		 taskUl.appendChild(taskDiv)
+
+	  }
 		
-	   const getTasks = () => {
-		   
-		   fetch(baseUrl)
-		   .then(resp => resp.json())
-		   .then(tasks => addTasks(tasks))
-	   } 
-
-	   const addTasks = (tasks) => {
-		   for(const task of tasks){
-			   addTaskToDom(task)
-		   }
-	   }
-
-	   const addTaskToDom = (task) => {
-		   const taskUl = document.querySelector('#list')
-		   const taskDiv = document.createElement('div')
-		   taskDiv.classList.add('task-div')
-		   taskDiv.dataset.id = task.id
-		   const taskP = document.createElement('p')
-		   taskP.innerHTML =`
-		   <h3>${task.subject}</h3>
-		   <p>${task.content}</p>
-		   <button data-id="${task.id}" class="delete">&times;</button>
-		   `
-		   	taskDiv.appendChild(taskP)
-			taskUl.appendChild(taskDiv)
-
-	   }
+	   
 
 	   const submitHandler = () => {
+		   
 		   document.addEventListener('submit', e => {
-			   e.preventDefault()
-			   const form = e.target
+			if(e.target.matches('.task-form')){
 
-			   const subject = form.subject.value
-			   const content = form.content.value 
-			   
-			   const newNote = {subject: subject, content: content}
-			   
+				e.preventDefault()
+				const form = e.target
+ 
+				const subject = form.subject.value
+				const content = form.content.value 
+				
+				const newNote = {subject: subject, content: content}
+				
+ 
+				const options = {
+					method: "POST",
+					headers: {
+						"content-type": "application/json",
+						"accept": "application/json"
+					},
+					body: JSON.stringify(newNote)
+				}
+ 
+				fetch(baseUrl, options)
+				.then(resp => resp.json())
+				.then(task => {addTaskToDom(task)})
+				 document.querySelector('.task-form').reset()
 
-			   const options = {
-				   method: "POST",
-				   headers: {
-					   "content-type": "application/json",
-					   "accept": "application/json"
-				   },
-				   body: JSON.stringify(newNote)
+				
+
+			}
+			else if(e.target.matches('.edit-form')){
+				e.preventDefault()
+				let editForm = e.target
+				let taskId = editForm.dataset.id
+
+				let subject = editForm.subject.value
+				let content = editForm.content.value
+				
+				let editedTask = {subject: subject, content: content}
+				
+				let options = {
+					method: "PATCH",
+					headers: {
+						"content-type": "application/json",
+						"accept": "application/json"
+					},
+					body: JSON.stringify(editedTask)
+				}
+
+				fetch(baseUrl + taskId, options)
+				.then(resp => resp.json())
+				.then(edit => addTaskToDom(edit))
+
+				
+				     let addTask = false
+
+					 let editContainer = document.querySelector('.edit-form')
+
+					 addTask = !addTask
+					 if (addTask) {
+					 editContainer.style.display = "none";
+					 } else {
+					 editFormContainer.style.display = "block";
+					 }
 			   }
-
-			   fetch(baseUrl, options)
-			   .then(resp => resp.json())
-			   .then(task => {addTaskToDom(task)})
-			    document.querySelector('.task-form').reset()
+			
+		  })
+		}
 		
-	   })
+		
+	   
+	
+		
 
-	}	
 		   const clickHandler = () => {
 			   document.addEventListener('click', e => {
 				if(e.target.matches('.delete')){
 					let button = e.target
 					let taskId = button.dataset.id
-					
 
-					
-					
 					let options = {
-						method: "DELETE"
+					 method: "DELETE"
 					}
 
 					fetch(baseUrl + taskId, options)
 					.then(resp => resp.json())
 					.then(_data => {
-						e.target.closest('div').remove()
+						e.target.parentElement.parentElement.remove()
 					})
-				}
-			   })
-		   }
+				 }
+				
+				else if(e.target.matches('.edit')){
+					 let edButton = e.target
+					 let taskId = edButton.dataset.id
+					 let subject = edButton.parentElement.children[0].textContent
+					 let content = edButton.parentElement.children[1].textContent
+	
+					 let form = document.querySelector('.edit-form')
+					
+					 form.subject.value = subject
+					 form.content.value = content
+
+					 form.dataset.id = taskId
+
+					let addTask = false;
+
+					let editContainer = document.querySelector('.edit-form')
+
+					addTask = !addTask;
+					if (addTask) {
+					editContainer.style.display = "block";
+					} else {
+					editFormContainer.style.display = "none";
+					}
+
+				}	 
+
+				  else if (e.target.matches('.seeNotes')){
+
+
+					const getTasks = () => {
+		   
+						fetch(baseUrl)
+						.then(resp => resp.json())
+						.then(tasks => addTasks(tasks))
+					} 
+			 
+					const addTasks = (tasks) => {
+						const taskUl = document.querySelector('#list')
+						taskUl.innerHTML = ""
+						for(const task of tasks){
+							addTaskToDom(task)
+						}
+					}
+			 
+					
+					  getTasks();
+
+					  let seebutton = document.querySelector('.seeNotes')
+					  seebutton.textContent = "Hide Tasks🤫"
+						
+
+					 }
+					 else if(e.target.innerText === "Hide Tasks🤫"){
+						 console.log('click')
+						 
+					 }
+
+			       }
+			      ) 
+                 } 
+			
 
 
 	   
 		clickHandler();
 		submitHandler();
-	    getTasks();
+	    
 
 
 })
